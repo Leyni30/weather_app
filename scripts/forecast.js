474d45a867c89c3869954fcd073854789039c856
Elena <elena_donceva@yahoo.com>
2020-06-04 00:08:36 -0400

    added the two functions within which with fetch() I use the API endpoints to get information about a city first then its weather conditions

---
+++ scripts/forecast.js
@@ -0,0 +1,37 @@
+const key = 'XqtPfmEYqiSie24HNwyAcuGGXHP49pEA';
+
+//Get weather information
+const getWeather = async (locationKey) => {
+    const weatherBase = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${locationKey}?apikey=${key}`;
+    const weather = await fetch(weatherBase + query);
+    const weatherData = await weather.json();
+    console.log(weatherData);
+    return weatherData[0];
+}
+
+
+
+
+// Get city information
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+
+    const query = `?apikey=${key}&q=${city}`;
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    //console.log(data);
+    return data[0];
+}
+
+getCity('San Francisco').then(data => {
+    return getWeather(data.Key);
+}).then(data => {
+    console.log(data);
+}).catch(err => console.log(err));
+
+/*The second .then() method is there
+ because the first one returns a promise
+  which we resolve in the second 
+  then are able to use the data.*/
